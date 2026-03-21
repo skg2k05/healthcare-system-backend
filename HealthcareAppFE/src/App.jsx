99b481a1e41ee9ba71bdc43402a4e94276b984a5
Sushil Kumar Gauda <sushilkumargauda@gmail.com>
2026-03-21 20:31:45 +0530

    integrating the frontend and backend server

--- HealthcareAppFE/src/App.jsx
+++ HealthcareAppFE/src/App.jsx
@@ -1,28 +1,17 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
 import CitizenDashboard from "./pages/CitizenDashboard";
 import DoctorDashboard from "./pages/DoctorDashboard";
 
 function App() {
-  const token = localStorage.getItem("token");
-
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-
-      <Route
-        path="/citizen"
-        element={token ? <CitizenDashboard /> : <Navigate to="/login" />}
-      />
-
-      <Route
-        path="/doctor"
-        element={token ? <DoctorDashboard /> : <Navigate to="/login" />}
-      />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/citizen-dashboard" element={<CitizenDashboard />} />
+        <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
