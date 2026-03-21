9b318e6c3004e7b424f87c85ac11f200c3be110f
Sushil Kumar Gauda <sushilkumargauda@gmail.com>
2026-03-21 12:44:16 +0530

    integrating the frontend and backend server

--- src/main/java/com/healthcare/healthcare_system/config/SecurityConfig.java
+++ src/main/java/com/healthcare/healthcare_system/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
+
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
